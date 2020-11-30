69f18d1f11398555f0390ebd23ce073c62e74b73
David Gershony <david@vsodir.onmicrosoft.com>
2020-11-30 17:44:22 +0000

    Ping pong processor without tests

---
+++ src/Lightning/Network/Protocol/INetworkPeerContext.cs
@@ -0,0 +1,20 @@
+using MithrilShards.Core.Network;
+using Network.Protocol.Transport;
+
+namespace Network.Protocol
+{
+   public interface INetworkPeerContext : IPeerContext
+   {
+      bool HandshakeComplete { get; set; }
+
+      bool InitComplete { get; set; }
+
+      IHandshakeProtocol HandshakeProtocol { get; set; }
+
+      void SetHandshakeProtocol(IHandshakeProtocol handshakeProtocol);
+
+      void OnHandshakeCompleted();
+
+      void OnInitMessageCompleted();
+   }
+}
---
+++ src/Lightning/Network/Protocol/Processors/PingPongProcessor.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MithrilShards.Core;
+using MithrilShards.Core.EventBus;
+using MithrilShards.Core.Network.PeerBehaviorManager;
+using MithrilShards.Core.Network.Protocol.Processors;
+using MithrilShards.Core.Threading;
+using Network.Protocol.Messages;
+
+namespace Network.Protocol.Processors
+{
+   public class PingPongProcessor : BaseProcessor,
+      INetworkMessageHandler<PingMessage>,
+      INetworkMessageHandler<PongMessage>
+   {
+      const int PING_INTERVAL_SECS = 60;
+      
+      private readonly IDateTimeProvider _dateTimeProvider;
+      private readonly IPeriodicWork _periodicWork;
+      readonly IRandomNumberGenerator _numberGenerator;
+      
+      private DateTime? _lastPingReceivedDateTime;
+      //private ushort? _lastPingBytesLen = null;
+      private readonly ConcurrentDictionary<ushort,TrackedPingMessage> _lastSentPingMessages = 
+         new ConcurrentDictionary<ushort, TrackedPingMessage>();
+      
+      public PingPongProcessor(ILogger<BaseProcessor> logger, IEventBus eventBus, 
+         IPeerBehaviorManager peerBehaviorManager, IDateTimeProvider dateTimeProvider, IPeriodicWork periodicWork, IRandomNumberGenerator numberGenerator) 
+         : base(logger, eventBus, peerBehaviorManager, true)
+      {
+         _dateTimeProvider = dateTimeProvider;
+         _periodicWork = periodicWork;
+         _numberGenerator = numberGenerator;
+      }
+
+      public async ValueTask<bool> ProcessMessageAsync(PingMessage message, CancellationToken cancellation) //TODO David add tests
+      {
+         logger.LogDebug($"Processing ping from {PeerContext.PeerId}");
+         
+         if(_lastPingReceivedDateTime > _dateTimeProvider.GetUtcNow()
+            .AddSeconds(-30))
+            throw new ProtocolViolationException("TODO David check how we fail a channel from here");
+
+         if (message.NumPongBytes > PingMessage.MAX_BYTES_LEN)
+            return false;
+
+         _lastPingReceivedDateTime = _dateTimeProvider.GetUtcNow();
+         
+         await SendMessageAsync(new PongMessage {BytesLen = message.NumPongBytes,
+            Ignored = new byte[message.NumPongBytes]}, cancellation)
+            .ConfigureAwait(false);
+
+         logger.LogDebug($"Send pong to {PeerContext.PeerId}");
+         
+         // will prevent to handle noise messages to other Processors
+         return false;
+      }
+
+      public ValueTask<bool> ProcessMessageAsync(PongMessage message, CancellationToken cancellation)
+      {
+         logger.LogDebug($"Processing pong from {PeerContext.PeerId}");
+         
+         if(_lastSentPingMessages.ContainsKey(message.BytesLen) && 
+            _lastSentPingMessages.Remove(message.BytesLen,out var trackedPingMessage))
+         {
+            logger.LogDebug($"Pong received for ping after {_dateTimeProvider.GetUtcNow() - trackedPingMessage.dateTimeSent} on" +
+                            $"{PeerContext.PeerId}");
+         }
+
+         return new ValueTask<bool>(false);
+      }
+
+      private async Task PingAsync(CancellationToken cancellationToken)
+      {
+         var bytesLength = _numberGenerator.GetUint32();
+       
+         while(bytesLength > PingMessage.MAX_BYTES_LEN || _lastSentPingMessages.ContainsKey((ushort)bytesLength))
+            bytesLength = _numberGenerator.GetUint32();
+         
+         var pingMessage = new PingMessage((ushort)bytesLength);
+
+         _lastSentPingMessages.GetOrAdd((ushort)bytesLength, _
+            => new TrackedPingMessage(pingMessage, _dateTimeProvider.GetUtcNow()));
+
+         logger.LogDebug($"Sending ping to {PeerContext.PeerId}");
+         
+         await SendMessageAsync(pingMessage, cancellationToken).ConfigureAwait(false);
+      }
+      
+      protected override ValueTask OnPeerHandshakedAsync()
+      {
+         _ = _periodicWork.StartAsync(
+            label: $"{nameof(_periodicWork)}-{PeerContext.PeerId}",
+            work: PingAsync,
+            interval: TimeSpan.FromSeconds(PING_INTERVAL_SECS),
+            cancellation: PeerContext.ConnectionCancellationTokenSource.Token
+         );
+
+         return default;
+      }
+
+      private class TrackedPingMessage
+      {
+         public PingMessage pingMessage;
+         public DateTime dateTimeSent;
+
+         public TrackedPingMessage(PingMessage pingMessage, DateTime dateTimeSent)
+         {
+            this.pingMessage = pingMessage;
+            this.dateTimeSent = dateTimeSent;
+         }
+      }
+   }
+}
