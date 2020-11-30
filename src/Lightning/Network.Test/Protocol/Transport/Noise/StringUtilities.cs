4360576c980a662de8e935975631876826ee3881
David Gershony <david@vsodir.onmicrosoft.com>
2020-11-30 11:44:24 +0000

    Added ping message and serializer

--- src/Lightning/Network.Test/Protocol/Transport/Noise/StringUtilities.cs
+++ src/Lightning/Network.Test/Protocol/Transport/Noise/StringUtilities.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Network.Test.Protocol.Transport.Noise
 {
@@ -16,6 +18,21 @@ namespace Network.Test.Protocol.Transport.Noise
                 .Where(x => x % 2 == 0)
                 .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                 .ToArray();
+        }
+
+        public static string ToHexString(this ReadOnlySpan<byte> span)
+        {
+           return span.ToArray().ToHexString();
+        }
+        
+        public static string ToHexString(this IEnumerable<byte> arr)
+        {
+           var sb = new StringBuilder();
+           sb.Append("0x");
+           foreach (byte b in arr)
+              sb.Append(b.ToString("X2"));
+
+           return sb.ToString().ToLower();
         }
     }
 }
