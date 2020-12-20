9b3894355345e1dc01966f25a7ce211b77f4e0b3
dangershony <dan.gershony@gmail.com>
2020-12-20 17:17:48 +0000

    Create primitives project

---
+++ src/Lightning/Bitcoin.Primitives/Serialization/Serializers/NetworkAddressSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Buffers;
+using MithrilShards.Chain.Bitcoin.Protocol.Types;
+using MithrilShards.Core.Network.Protocol.Serialization;
+
+namespace MithrilShards.Chain.Bitcoin.Protocol.Serialization.Serializers.Types
+{
+   public class NetworkAddressSerializer : IProtocolTypeSerializer<NetworkAddress>
+   {
+      public NetworkAddress Deserialize(ref SequenceReader<byte> reader, int protocolVersion, ProtocolTypeSerializerOptions? options = null)
+      {
+         return new NetworkAddress
+         {
+            // https://bitcoin.org/en/developer-reference#version
+            Time = DateTimeOffset.FromUnixTimeSeconds(reader.ReadUInt()),
+
+            Services = reader.ReadULong(),
+            IP = reader.ReadBytes(16).ToArray(),
+            Port = reader.ReadUShort()
+         };
+      }
+
+      public int Serialize(NetworkAddress typeInstance, int protocolVersion, IBufferWriter<byte> writer, ProtocolTypeSerializerOptions? options = null)
+      {
+         int size = 0;
+         // https://bitcoin.org/en/developer-reference#version
+         size += writer.WriteUInt((uint)typeInstance.Time.ToUnixTimeSeconds());
+
+         size += writer.WriteULong(typeInstance.Services);
+         size += writer.WriteBytes(typeInstance.IP!);
+         size += writer.WriteUShort(typeInstance.Port);
+
+         return size;
+      }
+   }
+}
---
+++ src/Lightning/Bitcoin.Primitives/Serialization/Serializers/TransactionWitnessSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Buffers;
+using MithrilShards.Chain.Bitcoin.Protocol.Types;
+using MithrilShards.Core.Network.Protocol.Serialization;
+
+namespace MithrilShards.Chain.Bitcoin.Protocol.Serialization.Serializers.Types
+{
+   public class TransactionWitnessSerializer : IProtocolTypeSerializer<TransactionWitness>
+   {
+      readonly IProtocolTypeSerializer<TransactionWitnessComponent> _transactionWitnessComponentSerializer;
+
+      public TransactionWitnessSerializer(IProtocolTypeSerializer<TransactionWitnessComponent> transactionWitnessComponentSerializer)
+      {
+         _transactionWitnessComponentSerializer = transactionWitnessComponentSerializer;
+      }
+
+      public TransactionWitness Deserialize(ref SequenceReader<byte> reader, int protocolVersion, ProtocolTypeSerializerOptions? options = null)
+      {
+         return new TransactionWitness
+         {
+            Components = reader.ReadArray(protocolVersion, _transactionWitnessComponentSerializer)
+         };
+      }
+
+      public int Serialize(TransactionWitness typeInstance, int protocolVersion, IBufferWriter<byte> writer, ProtocolTypeSerializerOptions? options = null)
+      {
+         return writer.WriteArray(typeInstance.Components, protocolVersion, _transactionWitnessComponentSerializer);
+      }
+   }
+}
